7c4d867ec9d97c53c0b5ffec73676f8533f8d0f4
Ysacc Roncal <samironcal@gmail.com>
2023-08-31 05:05:34 -0500

    changes filter

--- app/filter/page.js
+++ app/filter/page.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Head from 'next/head';
 import CharacterFilter from '../../components/CharacterFilter';
-
+import "./style.css"
 function FilterPage() {
   return (
     <div>
       <Head>
         <title>Character Filter</title>
       </Head>
-      <h1>Filtro de Personajes </h1>
+      <h1 className='title'>Filtro de Personajes </h1>
       <CharacterFilter />
     </div>
   );
--- components/SearchResults.js
+++ components/SearchResults.js
@@ -2,19 +2,19 @@ import React from 'react';
 import './styles.css'
 
 function SearchResults({ results }) {
-  return (
-    <div className='results'>
-      <h1 className='title'>Results Search</h1>
-      {results.map((result) => (
-        <div key={result.id}>
-          {/* <img className='img' src={result.image} alt={result.name} />  */}
-          <h1>NAME:{result.name}</h1>
-          <p>GENDER:{result.gender}</p>
-          <p>SPECIES:{result.species}</p>
-          <p>STATUS:{result.status}</p>
-        </div>
-      ))}
-    </div>
+  return (<></>
+    // <div className='results'>
+      
+    //   {results.map((result) => (
+    //     <div key={result.id}>
+    //       {/* <img className='img' src={result.image} alt={result.name} />  */}
+    //       <h1>NAME:{result.name}</h1>
+    //       <p>GENDER:{result.gender}</p>
+    //       <p>SPECIES:{result.species}</p>
+    //       <p>STATUS:{result.status}</p>
+    //     </div>
+    //   ))}
+    // </div>
   );
 }
 
